0adfa844df50e0ef080ffe37ca60f51f92d61c96
LivHed <livia.hedin@gmail.com>
2020-06-24 17:23:26 +0200

    Format code properly

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ var isPartyTime = false;
 var updateClock = function() 
 {
 
-    var timeOfDayText =     document.getElementById("timeEvent");
+    var timeOfDayText = document.getElementById("timeEvent");
     var lolcatImage = document.getElementById('lolcatImage');
     var messageText;
     var image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat5.jpg";
@@ -20,30 +20,30 @@ var updateClock = function()
     if (time == partyTime){
 	  messageText = "IZ PARTEE TIME!!";
 	  image = "https://www.bellrockgrowers.com/wp-content/uploads/2016/02/cat-party.jpg";
-}   else if (time == napTime) {
+    }   else if (time == napTime) {
 	  messageText = "IZ NAP TIME...";
 	  image = "https://i.redd.it/m9agulgygx331.jpg";
-}   else if (time == lunchTime) {
+    }   else if (time == lunchTime) {
 	  messageText = "IZ NOM NOM NOM TIME!!";
 	  image = "https://66.media.tumblr.com/c87f278c879cdfdd3fb6741f548b4768/tumblr_inline_p5dm1pdsDH1t7947e_540.jpg";
-}   else if (time == wakeupTime) {
+    }   else if (time == wakeupTime) {
 	  messageText = "IZ TIME TO GETTUP.";
 	  image = "https://www.iizcat.com/uploads/2016/02/99d4t-lp1.jpg";
-}   else if (time < noon) {
+    }   else if (time < noon) {
       messageText = "Good Morning! You are CATsome!";
 	  image = "https://i.redd.it/tfi7qlekduo31.jpg";
-}   else if (time > evening) {
+    }   else if (time > evening) {
       messageText = "Good Evening Hooman!";
 	  image = "https://i.pinimg.com/originals/8f/35/96/8f3596b39ed36dd4eba1a7c51fe21189.jpg";
-}   else {
+    }   else {
       messageText = "Good afternoon Hooman!";
 	  image = "https://i.pinimg.com/originals/ef/85/e5/ef85e55356c5ba89b5dc2bb234367fdf.jpg";
-}
+    }
  
-timeOfDayText.innerText = messageText;
-lolcatImage.src = image;
+    timeOfDayText.innerText = messageText;
+    lolcatImage.src = image;
 
-showCurrentTime();
+    showCurrentTime();
 
 };
 
@@ -99,22 +99,22 @@ var wakeUpTimeSelector = document.getElementById("wakeUpTimeSelector");
 // The text and color of the party button and the image with text are changing when you click the btn.
 var partyEvent = function() {
    
-   if (isPartyTime === false) {
+    if (isPartyTime === false) {
       isPartyTime = true;
       time = partyTime;
 	   
 	  partyTimeButton.innerText = "Party Time";
 	  partyTimeButton.style.backgroundColor = "#bc658d";
       
-   }
-   else {
+    }
+    else {
       isPartyTime = false;
       time = new Date().getHours();
 	   
 	   partyTimeButton.innerText = "Party Over!";
 	   partyTimeButton.style.backgroundColor = "#222";
       
-   }
+    }
 };
 
 // You can set the Time selectors, and with that the Lol Cat image and text 
